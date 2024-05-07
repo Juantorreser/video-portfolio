351827b0eccc492b66c6b6026bdbeb285cd232e0
Juan Torres <jdtorreserazo@gmail.com>
2024-05-07 13:40:14 -0700

    Hero v1 Done

--- src/Hero/hero.tsx
+++ src/Hero/hero.tsx
@@ -1,14 +1,23 @@
 import "./hero.scss";
+import heroImage from "../assets/hero/heroImage3.png";
 
 export default function Hero() {
   return (
     <section id="hero" className="h-screen">
-      <div className="p-6 flex w-full h-full  align-middle justify-around">
-        <div className="w-1/3 flex justify-center items-center">
-            <p>Hola</p>
+      <div
+        className="p-6 flex flex-col items-center h-full justify-center
+                      md:flex md:flex-row md:align-middle md:justify-around"
+      >
+        <div className="m-14 full flex flex-col justify-center items-center text-5xl font-semibold text-white">
+          <p className="m-4">Hey, I'm Torreser</p>
+          <p className="m-3">Video Editing</p>
         </div>
-        <div className="w-1/3 flex justify-center items-center">
-            <img src="" alt="Video Editing Services"/>
+        <div className="mt-14 md:w-1/3 flex  justify-center items-center">
+          <img
+            src={heroImage}
+            alt="Video Editing Services"
+            className="h-auto w-2/3 md:w-full"
+          />
         </div>
       </div>
     </section>
