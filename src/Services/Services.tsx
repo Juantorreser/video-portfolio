11a57c669e4229306bdec316efc7ffc07e83e0e3
Juan Torres <jdtorreserazo@gmail.com>
2024-05-08 23:49:00 -0700

    Shape Divider Added to Hero

--- src/Services/Services.tsx
+++ src/Services/Services.tsx
@@ -2,8 +2,8 @@ import "./Services.scss";
 
 export default function Services() {
     return(
-        <section id="services" className="h-screen">
-            a
+        <section id="services" className="h-screen pt-5">
+            <h1 className="text-center text-">Services</h1>
 
         </section>
     )
