d5aa32dd3c14e5f2c5a66d9093bcd9afc1c0627f
Juan Torres <jdtorreserazo@gmail.com>
2024-05-13 19:41:59 -0700

    Work Glass Section Created

--- src/Work/Work.tsx
+++ src/Work/Work.tsx
@@ -1,5 +1,11 @@
-export default function Work(){
-    return(
-        <div id="work" className="h-screen text-center">Work</div>
-    )
+import './Work.scss'
+
+export default function Work() {
+  return (
+    <div id="work" className="h-screen flex justify-center items-center">
+      <div id="portfolio" className='bg-ter'>
+        a
+      </div>
+    </div>
+  );
 }
