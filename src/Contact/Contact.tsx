bcbab71592835e6f3f050440fd7e68cf48b89b71
Juan Torres <jdtorreserazo@gmail.com>
2024-05-15 16:35:17 -0700

    Contact Styling Done

--- src/Contact/Contact.tsx
+++ src/Contact/Contact.tsx
@@ -2,15 +2,25 @@ import "./Contact.scss";
 
 export default function Contact() {
   return (
-    <div id="contactMe" className="h-screen">
+    <div id="contactMe" className="h-screen pt-2">
       <h1 className="sectionTitle">Contact Me</h1>
       <form action="">
-        <input type="email" id="formEmail" placeholder="Email" />
-        <input type="text" id="formName" placeholder="Name"/>
-        <input type="text" id="formBudget" placeholder="Budget (Optional)" />
-        <textarea name="formMessage" id="formMessage">Message</textarea>
+        <div className="col">
+          <input type="email" id="formEmail" placeholder="Email" />
+          <input type="text" id="formName" placeholder="Name" />
+          <input type="text" id="formBudget" placeholder="Budget (Optional)" />
+        </div>
+        <div className="col">
+          <textarea
+            name="formMessage"
+            id="formMessage"
+            placeholder="Message"
+          ></textarea>
+        </div>
       </form>
-      <button type="submit">Send Message</button>
+      <div className="row">
+        <button type="submit">Send Message</button>
+      </div>
     </div>
   );
 }
